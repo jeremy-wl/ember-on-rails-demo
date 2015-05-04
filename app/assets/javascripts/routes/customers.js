89a8df902c40e11174cd8d108fdf0993ec6c96bd
Jeremy Wong <linlin_19930805@yahoo.com>
2015-05-04 16:16:46 +0000

    Only make one Ajax call for customers

--- app/assets/javascripts/routes/customers.js
+++ app/assets/javascripts/routes/customers.js
@@ -1,5 +1,13 @@
 App.CustomersRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('customer');
+    var customersFromStore = this.store.all('customer');
+    // Use find method to fetch JSON over HTTP if there's no records available in local store
+    if (!customersFromStore.content.length) {
+      return this.store.find('customer');
+    }
+    // Otherwise use all method to return the records in the local store
+    else {
+      return customersFromStore;
+    }
   }
-})
+});
