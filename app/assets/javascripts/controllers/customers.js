cea8ade31aaa8933480eac285749d0c2639c1a18
Jeremy Wong <linlin_19930805@yahoo.com>
2015-05-05 13:33:28 +0000

    Filtering customers

--- app/assets/javascripts/controllers/customers.js
+++ app/assets/javascripts/controllers/customers.js
@@ -1,6 +1,16 @@
 App.CustomersController = Ember.ArrayController.extend({
   // sortProperties: ['ContactName'], // uncomment this to sort the list of customers by default
   sortAscending: false,
+  
+  filterText: '',
+
+  filteredResults: function() {
+    var filter = this.get('filterText');
+    return this.get('arrangedContent').filter(function(item, index, enumerable) {
+      return item._data.ContactName.toLowerCase().indexOf(filter) !== -1;
+    });
+  }.property('filterText', 'sortProperties'),
+      
   actions: {
     sortByName: function() {
       this.toggleProperty("sortAscending");
