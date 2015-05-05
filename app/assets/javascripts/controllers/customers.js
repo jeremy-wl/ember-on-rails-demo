3392feb89c6c98e4c4b6edf2d99f79e225799899
Jeremy Wong <linlin_19930805@yahoo.com>
2015-05-05 09:25:30 +0000

    Putting two pages in one; Add styling; Sorting customers

--- app/assets/javascripts/controllers/customers.js
+++ app/assets/javascripts/controllers/customers.js
@@ -1,7 +1,9 @@
 App.CustomersController = Ember.ArrayController.extend({
   // sortProperties: ['ContactName'], // uncomment this to sort the list of customers by default
+  sortAscending: false,
   actions: {
     sortByName: function() {
+      this.toggleProperty("sortAscending");
       this.set("sortProperties", ["ContactName"]);
     }
   }
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 App.Router.map(function() {
-   this.resource('customers');
-   this.resource('customer', {path: '/customer/:customerId'});
+   this.resource('customers', function() {
+      this.resource('customer', {path: '//:customerId'});
+   });
 });
